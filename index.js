cef1dfd86625aa64131eca835bac17fcf08cc465
Oendrila Chakraborty <oendrila@itobuz.com>
2024-12-26 18:29:54 +0530

    color-changer

---
+++ index.js
@@ -0,0 +1,13 @@
+const background = document.getElementById("background-colour");
+function aqua() {
+  background.style.backgroundColor = "aqua";
+}
+function gray() {
+  background.style.backgroundColor = "gray";
+}
+function blue() {
+  background.style.backgroundColor = "rgb(63, 113, 136)";
+}
+function green() {
+  background.style.backgroundColor = "aquamarine";
+}
